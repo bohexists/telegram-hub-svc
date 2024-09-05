5afda3d5e8a8d6f104049e901e15e915dea793fc
Bohdan Kyrylenko <kyrylenko.bohdan@gmail.com>
2024-09-05 20:20:42 +0100

    Add help_handler

---
+++ internal/handlers/help_handler.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"github.com/bohexists/telegram-hub-svc/pkg/telegram"
+	"log"
+)
+
+// HelpHandler sends a help message to the user
+func HelpHandler(client *telegram.Client, chatID int64) {
+	message := `Here are the available commands:
+/start - Start interacting with the bot
+/status - Get the current status of monitored cryptocurrencies
+/help - Show this help message`
+
+	err := client.SendMessage(chatID, message)
+	if err != nil {
+		log.Printf("Error sending help message: %v", err)
+	}
+}
